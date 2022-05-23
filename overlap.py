8910445e156fde42826b84a02f15dfe6bdd3d560
arejek <termiczny.kubek@gmail.com>
2022-05-23 17:23:50 +0200

    Correct mistakes in delta_lambda() function
    1) Wrong order of values
    2) Multiplication instead of division

--- overlap.py
+++ overlap.py
@@ -26,9 +26,9 @@ def delta_lambda(zeta_lbd_lbd, this_sp_shape_func, other_sp_shape_func, delta_g,
     delta_g_T = np.transpose(delta_g)
     matrix_M_inv = np.linalg.inv(matrix_M)
 
-    delta_lambda = this_sp_shape_func - other_sp_shape_func
+    delta_lambda = other_sp_shape_func - this_sp_shape_func
     delta_lambda = delta_lambda - np.dot(np.dot(delta_g_T, matrix_M_inv), nabla_of_both)
-    delta_lambda = (-1) * zeta_lbd_lbd * delta_lambda
+    delta_lambda = (-1)/zeta_lbd_lbd * delta_lambda
 
     return delta_lambda
 
