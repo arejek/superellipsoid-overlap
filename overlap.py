e92d47d0917bd8f3fbfea594b0cb54a1441eaca8
arejek <termiczny.kubek@gmail.com>
2022-05-24 13:05:39 +0200

    Rename variables
    Change names this_sp to sp_A and other_sp to sp_B.

--- overlap.py
+++ overlap.py
@@ -1,12 +1,12 @@
 import numpy as np
 
 
-def matrix_M(lbd, this_sp_nabla_2, other_sp_nabla_2):
-    return lbd * this_sp_nabla_2 + (1 - lbd) * other_sp_nabla_2
+def matrix_M(lbd, sp_A_nabla_2, sp_B_nabla_2):
+    return lbd * sp_A_nabla_2 + (1 - lbd) * sp_B_nabla_2
 
 
-def delta_g(this_sp_nabla, other_sp_nabla):
-    return this_sp_nabla - other_sp_nabla
+def delta_g(sp_A_nabla, sp_B_nabla):
+    return sp_A_nabla - sp_B_nabla
 
 
 def zeta_lbd_lbd(delta_g, matrix_M):
@@ -15,18 +15,18 @@ def zeta_lbd_lbd(delta_g, matrix_M):
     return np.dot(np.dot(delta_g_T, matrix_M_inv), delta_g)
 
 
-def nabla_of_both(lbd, this_sp_nabla, other_sp_nabla):
-    return lbd * this_sp_nabla + (1 - lbd) * other_sp_nabla
+def nabla_of_both(lbd, sp_A_nabla, sp_B_nabla):
+    return lbd * sp_A_nabla + (1 - lbd) * sp_B_nabla
 
 
-def delta_lambda(zeta_lbd_lbd, this_sp_shape_func, other_sp_shape_func, delta_g, matrix_M, nabla_of_both):
+def delta_lambda(zeta_lbd_lbd, sp_A_shape_func, sp_B_shape_func, delta_g, matrix_M, nabla_of_both):
 
     # (-1)/zeta_lbd_lbd * [(A.shape_func - B.shape_func) - delta_g^T * M^(-1) * nabla_of_both]
 
     delta_g_T = np.transpose(delta_g)
     matrix_M_inv = np.linalg.inv(matrix_M)
 
-    delta_lambda = other_sp_shape_func - this_sp_shape_func
+    delta_lambda = sp_B_shape_func - sp_A_shape_func
     delta_lambda = delta_lambda - np.dot(np.dot(delta_g_T, matrix_M_inv), nabla_of_both)
     delta_lambda = (-1)/zeta_lbd_lbd * delta_lambda
 
